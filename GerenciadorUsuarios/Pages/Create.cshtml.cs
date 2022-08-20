afe03b71ea9dc98b92f8a017c8edbe87e750634a
BrunaDan7as <b.dantassoares@gmail.com>
2022-08-19 22:18:54 -0300

    Tentando fazer a API funcionar.

--- GerenciadorUsuarios/Pages/Create.cshtml.cs
+++ GerenciadorUsuarios/Pages/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace GerenciadorUsuarios.Pages
 {
@@ -7,6 +8,19 @@ namespace GerenciadorUsuarios.Pages
     {
         public void OnGet()
         {
-        }
-    }
+            try
+            {  // CONEXÃO COM BANCO
+                string connectionString = "Data Source=DESKTOP-748SHP9\\SQLDANTAS;Initial Catalog=Gerenciador;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM Usuarios"; // sql query
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+
+    } }
 }
