22d6f12bf21e4c34b77a1f4946b601ebfe237a3e
BrunaDan7as <b.dantassoares@gmail.com>
2022-09-21 22:10:45 -0300

    Parte de inserir evoluindo.

--- GerenciadorUsuarios/Pages/Create.cshtml.cs
+++ GerenciadorUsuarios/Pages/Create.cshtml.cs
@@ -1,32 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Data.SqlClient;
+
 
 namespace GerenciadorUsuarios.Pages
 {
     public class CreateModel : PageModel
     {
+        public UsuariosInfo usuariosInfo = new UsuariosInfo();
+        public String errorMessage = "";
+        public String sucessMessage = "";
+
         public void OnGet()
         {
-            try
-            {  // CONEXÃO COM BANCO
-                string connectionString = "Data Source=DESKTOP-748SHP9\\SQLDANTAS;Initial Catalog=Gerenciador;Integrated Security=True";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string sql = "SELECT * FROM Usuarios"; // sql query
-                }
-            }
-            catch (Exception)
+
+        }
+
+        public void OnPost()
+        {
+            usuariosInfo.id = Request.Form["id"];
+            usuariosInfo.nome = Request.Form["nome"];
+            usuariosInfo.sobrenome = Request.Form["sobrenome"];
+            usuariosInfo.email = Request.Form["email"]; 
+            usuariosInfo.dataNascimento = Request.Form["dataNascimento"];
+            usuariosInfo.escolaridade = Request.Form["escolaridade"];
+
+            if (usuariosInfo.id == 0 || usuariosInfo.nome == 0 || usuariosInfo.sobrenome == 0 || usuariosInfo.email == 0 || usuariosInfo.dataNascimento == 0 || usuariosInfo.escolaridade == 0)
             {
+                errorMessage = "All the fields are required";
+                return;
 
             }
-            
-         
+
+            // salvar no banco
+
+            usuariosInfo.id = ""; usuariosInfo.nome = ""; usuariosInfo.sobrenome = ""; usuariosInfo.email = ""; usuariosInfo.dataNascimento = ""; usuariosInfo.escolaridade = "";
+
+
         }
-        protected void btncadastro(object sender, EventArgs e)
-        {
-            SqlCommand cmd = new SqlCommand();
-        }
-    }    
-}
+    }  
